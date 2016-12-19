8843591f36d3390f17af930b70b5039798539bad
Iegor Maiorov <maiorov.prog@gmail.com>
2016-12-18 17:15:39 -0800

    some experimetns with funcitons in es6

--- functions.js
+++ functions.js
@@ -10,3 +10,33 @@ var getPrice = (count, tax) => {
         price *= (1 + tax);
         return price;
 }
+
+var invoice = {
+    number : 123,
+    process : function() {
+        console.log(this);
+    }
+}
+
+invoice.process()
+
+
+
+// we don t have access to object on which function invoke when using arrow function syntax
+var invoiceWithArrow = {
+    number : 124,
+    process: () => console.log(this)
+}
+
+invoiceWithArrow.process();
+
+//
+
+var newInvoice = {
+    number: 456
+};
+// ??? what is bind - clarify
+// invoice.process().bind(newInvoice)();
+
+// what is call
+// invoice.process().call(newInvoice);
